e1eb5dfbadb31c397bf8a8d2e30754d1c5c72206
Ingrida <ingridakvetkovskyte@gmail.com>
2024-01-22 21:39:18 +0200

    fix double operator issue

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
 let result = [];
 let Number1;
 let Number2;
-let Operator;
+let operator;
 let currentValue = 0;
 let i = 0;
-let k = 0;
 let inputArr = [];
 document.querySelector(".dotBtn").disabled = true;
 document.querySelector(".equalBtn").disabled = true;
 disableOpBtn()
-
-
-
-
-
 
 
 const inputBtn = document.querySelectorAll('input');
@@ -24,21 +18,15 @@ const paragraph = document.querySelector('p')
     inputArr = [];
   }
 
-
-
-
-
   if (e.target.value == "AC"){
     paragraph.textContent = "";
     currentValue = 0;
     inputArr = [];
   } 
   
-  
   else if (e.target.value != "AC" && e.target.value != "DEL"){
     enableOpBtn()
     inputArr[i] = e.target.value; 
-
 
     if(inputArr[i]!=0){
       document.querySelector(".zeroBtn").disabled = false; 
@@ -47,16 +35,13 @@ const paragraph = document.querySelector('p')
           document.querySelector(".zeroBtn").disabled = true;   
         } else{
           document.querySelector(".zeroBtn").disabled = false;
-        }
-
-       
-    }
+        }   
+      }
 
 
     // Populate display with numbers
     let charArr;
     let stringToBeDisplayed;
-
     let string = inputArr.toString();
     charArr =string.replace(/,/g, "");
 
@@ -68,47 +53,46 @@ const paragraph = document.querySelector('p')
       paragraph.textContent = stringToBeDisplayed;
     }
 
-    // Getting Numbers
+    // Getting Numbers and Operator
     Number1 = parseFloat(charArr);
     let lengthToCut = Number1.toString().length+1;
     Number2 = parseFloat(charArr.slice(lengthToCut));  
 
-    console.log(typeof(charArr))
+    operator = String((charArr.slice(lengthToCut-1).slice(0, -2)));
 
-    let operator1 = String((charArr.slice(lengthToCut-1).slice(0, -1)));
+  
 
+    if(operator.length>1){
+      operator = operator.slice(0,-1);
+    }
 
+//???????
+    if(e.target.value == "-" && charArr.slice(-2) == "--"){
+      console.log("hi")
+      disableOpBtn();
+    }
 
-//getting operator not by event WORKS
-
-console.log(charArr)
-console.log("num1 "+Number1)
-console.log("num1 "+Number2)
-console.log("num3 "+operator1)
 
 
 
     // Decimal places and negative numbers
-if(charArr[0]=="-"){
-  document.querySelector(".subBtn").disabled = true;
-  disableOpBtn();
-
-}
-
+    if(charArr[0]=="-"){
+      document.querySelector(".subBtn").disabled = true;
+      disableOpBtn();
+    }
 
 
-if(typeof Number1==='number' && !isNaN(Number1)){
-    document.querySelector(".dotBtn").disabled = false;
-    document.querySelector(".equalBtn").disabled = true;
-    document.querySelector(".subBtn").disabled = false;
-    console.log("hooooo")
-    enableOpBtn();
-   
+
+    if(typeof Number1==='number' && !isNaN(Number1)){
+      document.querySelector(".dotBtn").disabled = false;
+      document.querySelector(".equalBtn").disabled = true;
+      document.querySelector(".subBtn").disabled = false;
+    
+      enableOpBtn();
 
       if(charArr.toString().includes(".")){
         document.querySelector(".dotBtn").disabled = true;
-     } 
-
+     ``} 
     }
 
     if(typeof Number2==='number' && !isNaN(Number2)){
@@ -125,11 +109,8 @@ if(typeof Number1==='number' && !isNaN(Number1)){
 
   //DEL
   else if (e.target.value == "DEL") {
-
-
     let stringToDisplay1= inputArr.toString().replace(/,/g, "").slice(0,-1)
     inputArr.pop();
-
 
       if(stringToDisplay1.length>9){
         paragraph.textContent = stringToDisplay1.slice((stringToDisplay1.length)-9);
@@ -139,56 +120,22 @@ if(typeof Number1==='number' && !isNaN(Number1)){
   } 
  
 
-
-
-/*
-Number1 = parseFloat(charArr);
-let lengthToCut = Number1.toString().length+1;
-Number2 = parseFloat(charArr.slice(lengthToCut));  
-
-
-*/
-
-
-
-
-  // Assign operator
-
-  if (e.target.value == "+"){
-    Operator = "+";
+  if (e.target.value == "+" || e.target.value == "x" || e.target.value == "/"){
     disableOpBtn();
-    //document.querySelector(".subBtn").disabled = true;
-
   }
 
-  if (e.target.value == "/"){
-    Operator = "/";
+
+  /*
+  if(e.target.value == "-" && charArr.slice(-2) == "--"){
     disableOpBtn();
-    //document.querySelector(".subBtn").disabled = true;
-  }
-
-  if (e.target.value == "-"){
-    Operator = "-";
-    disableOpBtn();
-//document.querySelector(".subBtn").disabled = true;
-  }
-
-  if (e.target.value == "x"){
-    Operator = "x";
-    disableOpBtn();
-
-    console.log(Number1)
-    console.log(Number2)
-   // document.querySelector(".subBtn").disabled = false;
-  } 
-
+  }*/
 
   // Do operations 
   if (e.target.value == "="){
 
     document.querySelector(".equalBtn").disabled = true;
 
-    if(Operator == "+"){
+    if(operator == "+"){
       let OpResult = Add(Number1,Number2);
       const paragraph = document. querySelector('p');
       paragraph.textContent = OpResult;
@@ -197,7 +144,7 @@ Number2 = parseFloat(charArr.slice(lengthToCut));
       inputArr[0] = OpResult;
     }
 
-    if(Operator == "-"){
+    if(operator == "-"){
       let OpResult = Substract(Number1,Number2);
       const paragraph = document. querySelector('p')
       paragraph.textContent = OpResult;
@@ -207,7 +154,7 @@ Number2 = parseFloat(charArr.slice(lengthToCut));
       
     }
   
-    if(Operator == "/"){
+    if(operator == "/"){
       let OpResult = Divide(Number1,Number2);
       const paragraph = document. querySelector('p')
       paragraph.textContent = OpResult;
@@ -216,7 +163,7 @@ Number2 = parseFloat(charArr.slice(lengthToCut));
       inputArr[0] = OpResult;
     }
     
-    if(Operator == "x"){
+    if(operator == "x"){
       let OpResult = Multiply(Number1,Number2);
       const paragraph = document. querySelector('p')
       paragraph.textContent = OpResult;
@@ -231,12 +178,6 @@ Number2 = parseFloat(charArr.slice(lengthToCut));
 
   i++;
 }))
-
-
-
-
-
-
 
 
 // Function Block
@@ -258,7 +199,6 @@ function enableOpBtn(){
 function Add(Number1,Number2) {
   let num1Digits = Number1.toString().split('').length;
   let num2Digits = Number2.toString().split('').length;
-
   let DigitMultiplier
 
   if(num1Digits>num2Digits){
@@ -275,7 +215,6 @@ function Add(Number1,Number2) {
 function Substract(Number1,Number2){
   let num1Digits = Number1.toString().split('').length;
   let num2Digits = Number2.toString().split('').length;
-
   let DigitMultiplier
 
   if(num1Digits>num2Digits){
@@ -291,7 +230,6 @@ function Substract(Number1,Number2){
 function Multiply(Number1,Number2) {
   let num1Digits = Number1.toString().split('').length;
   let num2Digits = Number2.toString().split('').length;
-
   let DigitMultiplier
 
   if(num1Digits>num2Digits){
@@ -305,12 +243,8 @@ function Multiply(Number1,Number2) {
 }
 
 function Divide(Number1,Number2) {
-
-
-
   let num1Digits = Number1.toString().split('').length;
   let num2Digits = Number2.toString().split('').length;
-
   let DigitMultiplier
 
   if(num1Digits>num2Digits){
@@ -324,9 +258,6 @@ function Divide(Number1,Number2) {
   if(Number2 == 0){
     division = "Error";
   }
-
-
-
 
   return division;
 }
